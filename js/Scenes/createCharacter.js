de9d2e5900cabbc48a91be33d8d496fdddf45468
gsayers <gsayers@discoverybenefits.com>
2017-01-09 16:17:22 -0600

    fixed?

--- js/Scenes/createCharacter.js
+++ js/Scenes/createCharacter.js
@@ -9,6 +9,8 @@ createChar.prototype = {
 
     create: function () {
 
+        var div = document.getElementById('gameForm');
+
         div.style.visibility = 'visible';
 
         var button = game.add.button(100, 150,'button', this.getData, this, 'g21776.png','g21766.png','g21786.png','g21766.png' );
@@ -27,7 +29,14 @@ createChar.prototype = {
 
         game.state.start("gameMain");
 
+        var div = document.getElementById('gameForm');
+
         div.style.visibility = 'hidden';
+
+        var viewportmeta = document.querySelector('meta[name="viewport"]');
+        if (viewportmeta) {
+            viewportmeta.setAttribute('content', 'width=device-width, maximum-scale=1.0, initial-scale=1.0');
+        }
 
     },
 
